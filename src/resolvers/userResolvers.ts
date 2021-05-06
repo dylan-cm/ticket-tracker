473418cf9008c1b89f8cda83945b9aefd4b59a62
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-05-06 13:00:53 -0700

    added queries to resolvers

--- src/resolvers/userResolvers.ts
+++ src/resolvers/userResolvers.ts
@@ -1,6 +1,6 @@
 import { IResolvers } from "apollo-server"
 import { CustomDataSourceType } from "../types/common/api"
-import { Scalars, Ticket, User } from "../types/generated"
+import { Event, Project, Scalars, Sprint, Ticket, User } from "../types/generated"
 
 const UserResolver: IResolvers = {
   Query: {
@@ -12,6 +12,13 @@ const UserResolver: IResolvers = {
       if(!userId) throw new Error('getUser query requires userId')
       
       return await dataSources.userAPI.getUser(userId)
+    },
+    getAllUsers: async (
+      _, 
+      __, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<User[]> => {
+      return await dataSources.userAPI.getAllUsers()
     },
     currentUser: async (
       _,
@@ -29,6 +36,76 @@ const UserResolver: IResolvers = {
       
       return await dataSources.userAPI.getUserTickets(userId)
     },
+
+    getProject: async (
+      _, 
+      { projectId }: {projectId: Scalars['ID']}, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Project | undefined> => {
+      if(!projectId) throw new Error('getUser query requires projectId')
+      
+      return await dataSources.userAPI.getProject(projectId)
+    },
+    getAllProjects: async (
+      _, 
+      __, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Project[]> => {
+      return await dataSources.userAPI.getAllProjects()
+    },
+    getProjectTickets: async (
+      _, 
+      { projectId }: {projectId: Scalars['ID']}, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Ticket[]> => {
+      if(!projectId) throw new Error('getUser query requires projectId')
+      
+      return await dataSources.userAPI.getProjectTickets(projectId)
+    },
+
+    getTicket: async (
+      _, 
+      { ticketId }: {ticketId: Scalars['ID']}, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Ticket | undefined> => {
+      if(!ticketId) throw new Error('getUser query requires ticketId')
+      
+      return await dataSources.userAPI.getTicket(ticketId)
+    },
+    getAllTickets: async (
+      _, 
+      __, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Ticket[]> => {
+      return await dataSources.userAPI.getAllTickets()
+    },
+    getTicketLog: async (
+      _, 
+      { ticketId }: {ticketId: Scalars['ID']}, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Event[]> => {
+      if(!ticketId) throw new Error('getUser query requires ticketId')
+      
+      return await dataSources.userAPI.getTicketLog(ticketId)
+    },
+
+    getSprint: async (
+      _, 
+      { sprintId }: {sprintId: Scalars['ID']}, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Sprint | undefined> => {
+      if(!sprintId) throw new Error('getUser query requires sprintId')
+      
+      return await dataSources.userAPI.getSprint(sprintId)
+    },
+    getAllSprints: async (
+      _, 
+      __, 
+      { dataSources }: {dataSources: CustomDataSourceType}
+    ): Promise<Sprint[]> => {
+      return await dataSources.userAPI.getAllSprints()
+    },
+
   },
 }
 
--- src/types/common/api.d.ts
+++ src/types/common/api.d.ts
@@ -1,11 +1,25 @@
-import { Ticket, User } from "../generated"
+import { Ticket, User, Project, Event, Sprint } from "../generated"
 
 interface CustomDataSourceType {
   userAPI: UserAPIProps
 }
 
 interface UserAPIProps {
-  getUser: (email?: string) => Promise<User | undefined>
+  getUser: (email: string) => Promise<User | undefined>
   currentUser: () => Promise<User | undefined>
-  getUserTickets: (email?: string) => Promise<Ticket[]>
+  getUserTickets: (email: string) => Promise<Ticket[]>
+  getAllUsers: () => Promise<[User]>
+
+  getProject: (projectId: string) => Promise<Project | undefined>
+  getAllProjects: () => Promise<[Project]>
+  getProjectTeam: (projectId: string) => Promise<[User]>
+  getProjectTickets: (projectId: string) => Promise<[Ticket]>
+  getProjectSprints: (projectId: string) => Promise<[Sprint]>
+
+  getTicket: (ticketId: string) => Promise<Ticket | undefined>
+  getAllTickets: () => Promise<[Ticket]>
+  getTicketLog: (ticketId: string) => Promise<[Event]>
+
+  getSprint: (sprintId: string) => Promise<Sprint | undefined>
+  getAllSprints: () => Promise<[Sprint]>
 }
